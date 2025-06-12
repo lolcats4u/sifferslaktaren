2f284c4415c1eb5bcac9dd07e06c84548d8e3569
Herl, Makenna <mherl@hrl.com>
2025-06-12 14:57:48 -0700

    A dumber lexer.

--- src/lexer.c
+++ src/lexer.c
@@ -24,10 +24,37 @@ static int read_number(Lexer *lexer) {
     return value;
 }
 
+Lexer lexer(char *input_str, int position, Token current_token){
+    int index = 0;
+    switch (input_str[index])
+    {
+    case ' ':
+        continue;
+    case '(':
+        current_node =  pack_single_node(input_str[index], (index + 1), TOKEN_LPAREN, &input_str[index+1]);
+    case ')':
+        current_node =  pack_single_node(input_str[index], (index + 1), TOKEN_RPAREN &input_str[index+1]);
+    case '+':
+        current_node = pack_single_node(input_str[index], (index + 1), TOKEN_PLUS);
+    case '-':
+        current_node = pack_single_node(input_str[index], (index + 1), TOKEN_MINUS);
+    case '*':
+        current_node = pack_single_node(input_str[index], (index + 1), TOKEN_MULTIPLICATION);
+    }
+}
+
+Lexer pack_single_node(char input, int position, Token current_token){
+    Lexer node;
+    node.input = input;
+    node.position = index + 1;
+    node.current_token = current_token;
+    return node;
+}
+
 void lexer_with_nested_mul(NestedLexer new_node, const char *input_str, int index){
     int start_index = index;
     StrSlice *substring;
-    substring->input_str;
+    substring->string_to_slice = input_str;
     while (input_str[index])
     {   if(start_index == 0){
             substring->left_index_inclusive = start_index;
@@ -36,62 +63,35 @@ void lexer_with_nested_mul(NestedLexer new_node, const char *input_str, int inde
             case ' ':
                 continue;
             case '(':
-                substring->right_index_exclusive = index;
-                new_node.prev = *substring;
-                Operator new_lparen;
-                new_lparen.operator = TOKEN_LPAREN;
-                int operator_priority = priority(input_str[index]);
-                new_lparen.priority = operator_priority;
+                pack_lexer(substring, new_node, TOKEN_LPAREN, 1, index, input_str);
             case ')':
-                substring->right_index_exclusive = index;
-                new_node.prev = *substring;
-                Operator new_rparen;
-                new_rparen.operator = TOKEN_RPAREN;
-                int operator_priority = priority(input_str[index]);
-                new_rparen.priority = operator_priority;
+                pack_lexer(substring, new_node, TOKEN_RPAREN, 1, index, input_str);
+            case '+':
+                pack_lexer(substring, new_node, TOKEN_PLUS, 0, index, input_str);
+            case '-':
+                pack_lexer(substring, new_node, TOKEN_MINUS, 1, index, input_str);
+            case '*':
+                pack_lexer(substring, new_node, TOKEN_MULTIPLICATION, 0, index, input_str);
             case '\0':
                 substring->right_index_exclusive = index;
                 new_node.prev = *substring;
-                new_node.operator = '\0'
-                new_node.next = '\0'
                 return new_node;
             }
         index++;
     }
 }
 
-void lexer_next_token(Lexer lexer, const char *input_str) {
-    if (lexer.to_the_left_left_index){
-        for (int i = lexer.to_the_left_left_index; i <= lexer.to_the_left_right_end_index, i++)
-        {
-            new_operation.right_left_index = lexer.to_the_left_left_index;
-            switch (input_str[i])
-            {
-            case '+':
-                lexer.new_operation.right_right_index = lexer.to_the_left_left_index;
-                lexer->current_token.type = TOKEN_PLUS;
-                lexer->position++;
-                break;
-            case '(':
-                lexer->current_token.type = TOKEN_LPAREN;
-                lexer->position++;
-                break;
-            case ')':
-                lexer->current_token.type = TOKEN_RPAREN;
-                lexer->position++;
-                break;
-            case '*':
-                lexer->current_token.type = TOKEN_MULTIPLICATION;
-                lexer->position++;
-            default:
-                lexer->current_token.type = TOKEN_ERROR;
-                lexer->position++;
-                break;
-        }
-        }
-    }
-
-
+NestedLexer pack_lexer(StrSlice *substring, NestedLexer new_node, int token_type, int order_matters, int index, const char *input_str)
+{
+    substring->right_index_exclusive = index;
+    new_node.prev = *substring;
+    Token new_token;
+    new_token.type = token_type;
+    Operator new_operator;
+    new_operator.operator = new_token;
+    int operator_priority = priority(input_str[index]);
+    new_operator.order_matters = order_matters;
+    new_operator.priority = operator_priority;
 }
 
 int priority(char operator){
